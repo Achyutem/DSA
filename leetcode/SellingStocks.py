cf45cc909aeb7d77143e81a26c7863b30322460e
achyutem <achyutem.ad@gmail.com>
2024-09-07 19:17:23 +0530

    updated selling stocks solution

--- leetcode/SellingStocks.py
+++ leetcode/SellingStocks.py
@@ -1,16 +1,19 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-
-
-class Solution(object):
+class Solution:
     def maxProfit(self, prices):
         l, r = 0, 1
-        maxP = 0
 
+        max_profit = 0
         while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                maxP = max(maxP, profit)
-            else:
+            if prices[l] > prices[r]:
                 l = r
-            r += 1
-        return maxP
+                r += 1
+            else:
+                profit = prices[r] - prices[l]
+
+                if profit > max_profit:
+                    max_profit = profit
+
+                r += 1
+
+        return max_profit
