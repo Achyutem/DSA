dc7ae44b320b43d2daaf5b700c208c5cc782e079
achyutem <achyutem.ad@gmail.com>
2024-09-15 17:05:46 +0530

    RPN

---
+++ leetcode/ReversePolishNotation.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        for i in tokens:
+            if i == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif i == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif i == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif i == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(i))
+        return stack[0]
