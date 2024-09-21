274f276b5ec9fc2cefa68ee16acb6fce80590d8a
achyutem <achyutem.ad@gmail.com>
2024-09-21 16:56:30 +0530

    reverse a linked list

---
+++ leetcode/ReverseLinkedList.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/reverse-linked-list
+
+
+class Solution:
+def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    prev, curr  = None, head
+
+    while curr:
+        temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
+    return prev
