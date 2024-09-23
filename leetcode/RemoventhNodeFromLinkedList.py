b6830815b8c72d7de45ec6f1fac205814e93c8ef
achyutem <achyutem.ad@gmail.com>
2024-09-23 19:01:21 +0530

    Remove nth Node From Linked List

---
+++ leetcode/RemoventhNodeFromLinkedList.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/remove-nth-node-from-end-of-list
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+
+        left = dummy
+        right = head
+
+        while n > 0:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+        return dummy.next
