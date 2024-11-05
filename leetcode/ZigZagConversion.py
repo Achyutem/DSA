a673c47033d5120c82980fb6433b68a8d70cf6ea
achyutem <achyutem.ad@gmail.com>
2024-11-05 19:43:01 +0530

    zigzag conversion updated

--- leetcode/ZigZagConversion.py
+++ leetcode/ZigZagConversion.py
@@ -8,15 +8,15 @@ class Solution(object):
 
         rows = [""] * numRows
         current_row = 0
-        going_down = False
+        direction = 1
 
         for char in s:
             rows[current_row] += char
             if current_row == 0:
-                going_down = True
+                direction = 1
             elif current_row == numRows - 1:
-                going_down = False
+                direction = -1
 
-            current_row += 1 if going_down else -1
+            current_row += direction
 
         return "".join(rows)
