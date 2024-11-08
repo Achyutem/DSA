8dce1890205af991df4a36055a25a0b0c679b13c
achyutem <achyutem.ad@gmail.com>
2024-11-08 20:07:26 +0530

    updated solution

--- leetcode/LongestSubstringWithoutRepeatingCharacters.py
+++ leetcode/LongestSubstringWithoutRepeatingCharacters.py
@@ -1,21 +1,35 @@
-#https://leetcode.com/problems/longest-substring-without-repeating-characters/
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+# class Solution:
+#     def lengthOfLongestSubstring(self, s):
+#         arr = []
+#         max_length = 0
+#
+#         for i in s:
+#             if i in arr:
+#                 while arr and arr[0] != i:
+#                     arr.pop(0)
+#                 arr.pop(0)
+#             arr.append(i)
+#             max_length = max(max_length, len(arr))
+#
+#         return max_length
+
 
 class Solution:
-    def lengthOfLongestSubstring(self, s):
-        arr = []
-        max_length = 0
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = 0
+        longest = 0
+        sett = set()
+        n = len(s)
 
-        for i in s:
-            if i in arr:
-                while arr and arr[0] != i:
-                    arr.pop(0)
-                arr.pop(0)
-            arr.append(i)
-            max_length = max(max_length, len(arr))
+        for r in range(n):
+            while s[r] in sett:
+                sett.remove(s[l])
+                l += 1
 
-        return max_length
+            w = (r - l) + 1
+            longest = max(longest, w)
+            sett.add(s[r])
 
-
-soln = Solution()
-s = "abcabcbb"
-print(soln.lengthOfLongestSubstring(s))
+        return longest
