dc1fd3c8b51943cf37e2cbc1b1cc13a88039d3e7
achyutem <achyutem.ad@gmail.com>
2024-12-15 21:06:37 +0530

    maximum average subarray

---
+++ leetcode/maximumAverageSubarrayI.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/maximum-average-subarray-i
+
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        n = len(nums)
+        currSum = 0
+
+        for i in range(k):
+            currSum += nums[i]
+
+        maxAvg = currSum / k
+
+        for i in range(k, n):
+            currSum += nums[i]
+            currSum -= nums[i - k]
+
+            avg = currSum / k
+
+            maxAvg = max(avg, maxAvg)
+
+        return maxAvg
