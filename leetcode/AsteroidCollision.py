85093acb9de4e841db2420944077305aa09a0922
achyutem <achyutem.ad@gmail.com>
2024-12-19 17:28:55 +0530

    asteroid collision

---
+++ leetcode/AsteroidCollision.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/asteroid-collision
+
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+
+        stack = []
+
+        for i in asteroids:
+            while stack and i < 0 and stack[-1] > 0:
+                if abs(i) > stack[-1]:
+                    stack.pop()
+                    continue
+                elif abs(i) == stack[-1]:
+                    stack.pop()
+                break
+
+            else:
+                stack.append(i)
+
+        return stack
